0978ef8bff1df9e70a2e039e17aaa83774277ee4
168496 <168496@LT078357.cts.com>
2020-08-25 13:09:58 +0530

    Syncing local project into remote repository

---
+++ JavaBasics/src/main/java/printValues/PrintSomething.java
@@ -0,0 +1,13 @@
+package printValues;
+
+public class PrintSomething {
+
+	/*
+	 * Goal of this program is to print Hello World
+	 */
+	public static void main(String[] args) {
+		// print some text Hello world
+		System.out.println("Hello World");   // in built method
+	}
+
+}
