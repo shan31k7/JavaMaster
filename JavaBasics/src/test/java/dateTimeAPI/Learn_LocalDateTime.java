d2ad58e05dc7cdecc58a531e57c00ad0d6af3e3f
168496 <168496@LT078357.cts.com>
2020-09-09 11:33:47 +0530

    Updated

--- JavaBasics/src/test/java/dateTimeAPI/Learn_LocalDateTime.java
+++ JavaBasics/src/test/java/dateTimeAPI/Learn_LocalDateTime.java
@@ -2,43 +2,40 @@ package dateTimeAPI;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
 
 import org.testng.annotations.Test;
-
+// Date
+// Calendar
 // immutable - cannot edit, pass by value - 
 // mutable - can be edited, pass by reference - 
 
-//Date
-//Calendar
 public class Learn_LocalDateTime {
 
 	@Test
 	public void demo_LocalDate() {
-		LocalDate local = LocalDate.of(2020, 2, 20);
-		System.out.println(local);
+		LocalDate today = LocalDate.now();
+		System.out.println(today);
+
+		LocalDate dob = LocalDate.of(1990, 2, 27);
+		System.out.println(ChronoUnit.YEARS.between(dob,today));
+		
+		System.out.print(dob.getDayOfWeek());
+		
 	}
 
 	@Test
-	public void demo_LocalTime() {
-		LocalTime time = LocalTime.now();
-
-		System.out.println(time);
-
-	}
-
-	@Test
-	public void demo_Calendar() {
-
-		Calendar calendar = Calendar.getInstance();
-		System.out.println(calendar.getTime());
-
-		if (calendar.before(calendar.getTime())) {
-
-		}
-
-		Date date = new Date();
+	public void demo_LocalTime() throws InterruptedException {
+		LocalTime time1 = LocalTime.now();
+		Thread.sleep(2000);
+		LocalTime time2 = LocalTime.now(ZoneId.of("US/Pacific"));
+		System.out.println(ChronoUnit.MILLIS.between(time1, time2));
+		
+		Set<String> zones = ZoneId.getAvailableZoneIds();
+		
+		zones.forEach(System.out::println);
 
 	}
 }
--- JavaBasics/src/test/java/streams/LearnStreams.java
+++ JavaBasics/src/test/java/streams/LearnStreams.java
@@ -3,15 +3,22 @@ package streams;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import javax.swing.plaf.synth.SynthSeparatorUI;
 
 public class LearnStreams {
 
+	// Collections - store huge set of data
+	// processing - filter, change it format,
+
+	// what to do ? but gives flexibility to design your filter
 	// stream - abstract - Collections and Array
 
 	public static ArrayList<String> fileHandle() throws FileNotFoundException {
@@ -30,26 +37,103 @@ public class LearnStreams {
 
 		ArrayList<String> activities = fileHandle();
 
-		// filter out the activity "Project Management", , first word
+		// filter out the activity "Project Management", upper case, split it and
+		// print first word
 
-		// collect
-		List<String> processed = activities.stream().filter((input) -> input.equalsIgnoreCase("Project Management"))
-				.map(input -> input.toLowerCase()).map(input -> input.split(" ")[0]).collect(Collectors.toList());
-		System.out.println("Filtered & collect ");
-		processed.forEach(i -> System.out.println(i));
+		List<String> filtered = activities.stream().filter((input) -> input.equalsIgnoreCase("Project Management"))
+				.map((input) -> input.toUpperCase()).map((input) -> input.split(" ")[0]).collect(Collectors.toList());
+
+		List<String> filtered1 = activities.stream().filter((input) -> input.equalsIgnoreCase("Project Management"))
+				.map((input) -> {
+					input = input.toUpperCase();
+					input = input.split(" ")[0];
+					return input;
+				}).collect(Collectors.toList());
+
+		// distinct values
+		activities.stream().distinct().collect(Collectors.toList());
+		activities.stream().collect(Collectors.toSet());
+
+		// orElse
+		activities.stream().filter((input) -> input.equalsIgnoreCase("Project Management and Leadership"))
+				.map((input) -> {
+					input = input.toUpperCase();
+					input = input.split(" ")[0];
+					return input;
+				}).collect(Collectors.toList());
+
+		// grouping and finding occurences
+		System.out.println("Grouping");
+		Map<String, Long> groupbyCount = activities.stream()
+				.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+		groupbyCount.forEach((value,count) -> System.out.println(value + " > " + count));
+		
+		
+		ArrayList<Integer> value = new ArrayList<Integer>();
+
+		value.add(34);
+		value.add(44);
+
+		value.add(555);
+
+		value.add(333);
+
+		value.add(77);
+		value.add(888);
+		System.out.println("Filtering the numbers");
+		Optional<Integer> filter = value.stream().filter(i -> i > 5000).findFirst();
+		System.out.print(filter.orElse(0));
+
+		System.out.println("Minum value" + value.stream().min(Comparator.naturalOrder()));
+
+		// Method reference
+		// pass by value - primitive types
+		// pass by reference - Objects
+		// pass by Method
+		// filtered.forEach(i-> System.out.println(i));
+		filtered.forEach(System.out::println);
 
 		// findAny
-		Optional<String> optional = activities.stream().filter((input) -> input.equalsIgnoreCase("Project Management"))
-				.map(input -> input.toLowerCase()).map(input -> input.split(" ")[0]).findAny();
-
-		System.out.println("Optional: " + optional.get());
-
+		/*
+		 * Optional<String> optional = activities.stream().filter((input) ->
+		 * input.equalsIgnoreCase("Project Management")) .map(input ->
+		 * input.toLowerCase()).map(input -> input.split(" ")[0]).findAny();
+		 * 
+		 * System.out.println("Optional: " + optional.get());
+		 */
 		// forEach
-		System.out.println("forEach: " + optional.get());
-
-		activities.parallelStream().filter((input) -> input.equalsIgnoreCase("Project Management"))
-				.map(input -> input.toLowerCase()).map(input -> input.split(" ")[0])
-				.forEach(i -> System.out.println(i));
+		/*
+		 * System.out.println("forEach: " + optional.get());
+		 * 
+		 * activities.parallelStream().filter((input) ->
+		 * input.equalsIgnoreCase("Project Management")) .map(input ->
+		 * input.toLowerCase()).map(input -> input.split(" ")[0]) .forEach(i ->
+		 * System.out.println(i));
+		 */
+		
+		
+		// flat map - 
+		
+		// nested collections
+		// normal collections as nested collection
+		
+		Set<String> names = new HashSet<String>();
+		
+		names.add("Raju");
+		names.add("Raju1");
+		names.add("Raju2");
+		names.add("Ramu");
+		names.add("Raju3");
+		names.add("Raju4");
+		names.add("Raju5");
+		names.add("Raju6");
+		names.add("Raju7");
+		names.add("Raju8");
+		
+		//List<String> flatmap = names.stream().flatMap(i -> Stream.of(i.charAt(0)).
+		
+		
 
 	}
 
